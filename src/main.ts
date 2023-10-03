6c84ca9f92bbaf9ae03107d675ede9b01f10cec8
Syed Hashir Ali Shah <hashishah00@gmail.com>
2023-10-03 15:03:49 +0500

    added cors true

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   // ensuring all endpoints are protected from receiving incorrect data.
   app.useGlobalPipes(new ValidationPipe());
 
